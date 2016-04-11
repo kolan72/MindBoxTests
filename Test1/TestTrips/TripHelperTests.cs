17527f45f8a0f132115be0bb3beef3a0b8381f22
kolan72 <kolan72@gmail.com>
2016-04-11 10:43:37 +0300

    Add Test1

---
+++ Test1/TestTrips/TripHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trips;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Trips.Tests
+{
+    [TestClass()]
+    public class TripHelperTests
+    {
+        [TestMethod()]
+        public void SortCards_Really_Sorted()
+        {
+            List<Tuple<string, string>> cards = new List<Tuple<string, string>>()
+            {
+                new Tuple<string, string>("Мельбурн", "Кельн"),
+                new Tuple<string, string>("Париж" , "Вена"),
+                new Tuple<string, string>("Москва" , "Париж"),
+                new Tuple<string, string>("Кельн" , "Москва")
+            };
+
+            var sortedCards = (new TripHelper()).SortCards(cards).ToList();
+
+            var n = Enumerable.Range(0, sortedCards.Count() - 1)
+                              .Where(i =>sortedCards[i].Item2 != sortedCards[i + 1].Item1)
+                              .ToList();
+
+            Assert.AreEqual(n.Count(), 0);
+        }
+
+
+        [TestMethod()]
+        public void SortCards_Counts_TheSame()
+        {
+            List<Tuple<string, string>> cards = new List<Tuple<string, string>>()
+            {
+                new Tuple<string, string>("Мельбурн", "Кельн"),
+                new Tuple<string, string>("Париж" , "Вена"),
+                new Tuple<string, string>("Москва" , "Париж"),
+                new Tuple<string, string>("Кельн" , "Москва")
+            };
+
+            Assert.AreEqual(cards.Count, (new TripHelper()).SortCards(cards).Count());
+        }
+    }
+}
---
+++ Test1/Trips/TripHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trips
+{
+    public class TripHelper
+    {
+        public IEnumerable<Tuple<string, string>> SortCards(IEnumerable<Tuple<string, string>> cards)
+        {
+            Dictionary<string, string> startPoints = new Dictionary<string, string>(cards.Count() - 2);
+            Dictionary<string, string> endPoints = new Dictionary<string, string>(cards.Count() - 2);
+
+            var ll = new LinkedList<string>();
+            var firstCard = cards.FirstOrDefault();
+            ll.AddFirst(firstCard.Item1);
+            ll.AddLast(firstCard.Item2);
+
+            foreach (var card in cards.Skip(1))
+            {
+                if (ll.Last.Value == card.Item1)
+                {
+                    ll.AddLast(card.Item2);
+                }
+                else if (ll.First.Value == card.Item2)
+                {
+                    ll.AddFirst(card.Item1);
+                }
+                else
+                {
+                    startPoints.Add(card.Item1, card.Item2);
+                    endPoints.Add(card.Item2, card.Item1);
+                }
+            }
+
+            bool tripStartFound = startPoints.Count == 0;
+            bool tripEndFound = endPoints.Count == 0;
+
+            if (tripStartFound && tripEndFound)
+            {
+                return cards;
+            }
+
+            while (!(tripStartFound && tripEndFound))
+            {
+                if (!tripStartFound)
+                {
+                    //          Проверяем существование конечного узла, совпадающего с уже добавленным в список начальным
+                    if (endPoints.ContainsKey(ll.First.Value))
+                    {
+                        ll.AddFirst(endPoints[ll.First.Value]);
+                    }
+                    else
+                    {
+                        tripStartFound = true;
+                    }
+                }
+                if (!tripEndFound)
+                {
+                    //          Проверяем существование начального узла, совпадающего с уже добавленным  в список конечным
+                    if (startPoints.ContainsKey(ll.Last.Value))
+                    {
+                        ll.AddLast(startPoints[ll.Last.Value]);
+                    }
+                    else
+                    {
+                        tripEndFound = true;
+                    }
+                }
+
+            }
+
+            return SplitToCards(ll);
+
+        }
+
+        private IEnumerable<Tuple<string, string>> SplitToCards(LinkedList<string> ll)
+        { 
+            LinkedListNode<string> f = ll.First;
+            List<Tuple<string, string>> tuples = new List<Tuple<string, string>>(ll.Count - 1);
+
+            while (f.Next != null)
+            {
+                tuples.Add(new Tuple<string, string>(f.Value, f.Next.Value));
+                f = f.Next;
+            }
+            return tuples;
+        }
+    }
+}
